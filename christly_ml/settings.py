eea70332ec33e8d7f44dfb037c08d410b0fb05f9
unworthyslaveTOCHRIST <righteousnessbyfaithinjesus@gmail.com>
2025-04-25 01:57:55 +0100

    Graciously bringing in vercel

--- christly_ml/settings.py
+++ christly_ml/settings.py
@@ -12,9 +12,14 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATICFILES_DIRS = [BASE_DIR/'static',]
+STATIC_ROOT = BASE_DIR/'staticfiles'
+
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
--- christly_ml/wsgi.py
+++ christly_ml/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "christly_ml.settings")
 
 application = get_wsgi_application()
+app = application
